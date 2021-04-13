648cbfaf311f63ec238b79375fb80b4e48b66e7e
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-04-12 22:11:08 -0300

    Stack and Queue examples - Alura Course - Collection 01

---
+++ Stack/Browse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    internal class Browse
+    {
+        private string actual = "vazia";
+        private readonly Stack<string> historyPrevious = new Stack<string>();
+        private readonly Stack<string> historyNext = new Stack<string>();
+
+        public Browse()
+        {
+            Console.WriteLine($"Actual page: {actual}");
+        }
+
+        internal void NavigateTo(string url)
+        {
+            historyPrevious.Push(actual);
+            actual = url;
+
+            Console.WriteLine($"Actual page: {actual}");
+        }
+
+        internal void ReturnPage()
+        {
+            if (historyPrevious.Any())
+            {
+                historyNext.Push(actual);
+                actual = historyPrevious.Pop();
+                Console.WriteLine($"Actual page: {actual}");
+            }
+        }
+
+        internal void NextPage()
+        {
+            if (historyNext.Any())
+            {
+                historyPrevious.Push(actual);
+                actual = historyNext.Pop();
+                Console.WriteLine($"Actual page: {actual}");
+            }
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+
+        static Queue<string> toll = new Queue<string>();
+
+        static void Main(string[] args)
+        {
+            #region Stack
+            //var browse = new Browse();
+            //browse.NavigateTo("google.com");
+            //browse.NavigateTo("caelum.com.br");
+            //browse.NavigateTo("alura.com.br");
+
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+
+            //browse.NextPage();
+            //browse.NextPage();
+            //browse.NextPage();
+            //browse.NextPage();
+            //browse.NextPage();
+
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            //browse.ReturnPage();
+            #endregion
+
+            #region Queue
+
+
+            TollEnqueue("Van");
+            TollEnqueue("Kombi");
+            TollEnqueue("Guincho");
+            TollEnqueue("Pickup");
+            TollDequeue();
+            TollDequeue();
+            TollDequeue();
+            TollDequeue();
+            TollDequeue();
+
+
+            #endregion
+
+
+            Console.ReadKey();
+
+        }
+
+        private static void TollDequeue()
+        {
+            
+            if (toll.Any())
+            {
+                if (toll.Peek() == "Guincho")
+                {
+                    Console.WriteLine($"Guincho is making payment");
+                }
+                Console.WriteLine("_________________________");
+                string vehicle = toll.Dequeue();
+                Console.WriteLine($"Dequeue: {vehicle}");
+                PrintQueue();
+            }
+            else
+            {
+                Console.WriteLine("The toll is empty");
+            }
+        }
+
+
+        private static void TollEnqueue(string vehicle)
+        {
+            Console.WriteLine("=======================");
+            toll.Enqueue(vehicle);
+            Console.WriteLine($"Into in queue: {vehicle}");
+            Console.WriteLine(".......................");
+            Console.WriteLine($"Queue:");
+            PrintQueue();
+        }
+
+        private static void PrintQueue()
+        {
+            foreach (var item in toll)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
